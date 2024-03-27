6a55d850aaf016eb24bf6323e8cd65737e1eeb42
Luke Wang <hw334@duke.edu>
2024-03-27 11:27:58 -0400

    intial test

--- src/example.py
+++ src/example.py
@@ -1,21 +1,4 @@
-"""Test example.
+"""A library that facilitate Discrete Neural Network Survival Analaysis.
 
-Other things.
+The function
 """
-
-
-def main() -> None:
-    """Run the example."""
-    pass
-
-
-def do_something() -> int:
-    """Do something else."""
-    a = 5
-    b = a * 3
-    c = a + b + a + b + a + b + a + b + a + b + a + b + a + b + a + b + a + b
-    return c
-
-
-if __name__ == "__main__":
-    main()
